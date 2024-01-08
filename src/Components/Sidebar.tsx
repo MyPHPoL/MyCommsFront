a7e7ae510230b553f3494b7cc1f2170b9df2394c
piotus <piotrradziszewski@outlook.com>
2024-01-08 10:46:38 +0100

    messages mockup

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Link, Routes } from "react-router-dom";
-import { IoAdd } from "react-icons/io5";
+import { IoMdAdd } from "react-icons/io";
 import { IoMdCreate } from "react-icons/io";
 import Server from "./Server";
 
@@ -11,8 +11,8 @@ interface SidebarProps {
 export default function Sidebar({ items }: SidebarProps) {
   return (
     <div>
-      <div className="fixed h-full top-20 left-0 w-31 flex flex-col bg-primary shadow-lg">
-        <ul className="m-4 first:my-0 last:my-0">
+      <div className='fixed h-full top-20 left-0 w-31 flex flex-col bg-primary'>
+        <ul className='m-4 first:my-0 last:my-0'>
           {items.map(({ id, name, picture }) => (
             <li key={id}>
               <Link to={id}>
@@ -21,10 +21,10 @@ export default function Sidebar({ items }: SidebarProps) {
             </li>
           ))}
         </ul>
-        <hr className="w-14 h-1 mx-auto my-4 border-0 rounded bg-gray-700"></hr>
-        <ul className="m-4 first:my-0 last:my-0">
+        <hr className='w-14 h-1 mx-auto my-4 border-0 rounded bg-gray-700'></hr>
+        <ul className='m-4 first:my-0 last:my-0'>
           <i>
-            <IconButton icon={<IoAdd />} name={"Join Server"} />
+            <IconButton icon={<IoMdAdd />} name={"Join Server"} />
           </i>
           <i>
             <IconButton icon={<IoMdCreate />} name={"Create Server"} />
@@ -32,7 +32,7 @@ export default function Sidebar({ items }: SidebarProps) {
         </ul>
       </div>
       <Routes>
-        <Route path="/:ServerId/*" element={<Server />} />
+        <Route path='/:ServerId/*' element={<Server />} />
       </Routes>
     </div>
   );
@@ -45,10 +45,10 @@ interface IconButtonProps {
 
 // technical buttons (join server, create server, etc.)
 const IconButton = ({ icon, name }: IconButtonProps) => (
-  <div className="font-semibold relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto bg-secondary group hover:bg-yellow-500 text-white hover:text-primary hover:rounded-xl rounded-3xl transition-all duration-300 ease-linear cursor-pointer shadow-lg;">
+  <div className='font-semibold relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto bg-secondary group hover:bg-yellow-500 text-white hover:text-primary hover:rounded-xl rounded-3xl transition-all duration-300 ease-linear cursor-pointer'>
     {icon}
 
-    <span className="group-hover:scale-100 z-50 absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md text-white bg-gray-900 text-xs font-bold transition-all duration-100 scale-0 origin-left;">
+    <span className='group-hover:scale-100 z-50 absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md text-white bg-gray-900 text-xs font-bold transition-all duration-100 scale-0 origin-left'>
       {name}
     </span>
   </div>
@@ -79,21 +79,19 @@ export var colors = [
 // buttons for servers or friends (dynamically changing background color or server picture as background)
 const ServerButton = ({ name, picture }: ServerButtonProps) => (
   <div
-    className="group font-semibold relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto text-primary hover:rounded-xl rounded-3xl transition-all duration-300 ease-linear cursor-pointer shadow-lg;"
+    className='group font-semibold relative flex items-center justify-center h-12 w-12 mt-2 mb-2 mx-auto text-primary hover:rounded-xl rounded-3xl transition-all duration-300 ease-linear cursor-pointer'
     style={{
       backgroundColor: colors[Math.floor(Math.random() * colors.length)],
-    }}
-  >
+    }}>
     {picture ? (
       <picture
-        className="group bg-center bg-cover w-full h-full rounded-full hover:bg-center hover:bg-cover hover:w-full hover:h-full"
-        style={{ backgroundImage: `url(${picture})` }}
-      ></picture>
+        className='group bg-center bg-cover w-full h-full rounded-full hover:bg-center hover:bg-cover hover:w-full hover:h-full'
+        style={{ backgroundImage: `url(${picture})` }}></picture>
     ) : (
       name[0].toUpperCase()
     )}
 
-    <span className="group-hover:scale-100 absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md text-white bg-gray-900 text-xs font-semibold transition-all duration-100 scale-0 origin-left;">
+    <span className='group-hover:scale-100 absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md text-white bg-gray-900 text-xs font-semibold transition-all duration-100 scale-0 origin-left'>
       {name}
     </span>
   </div>
