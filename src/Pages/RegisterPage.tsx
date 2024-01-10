443b71ebd0eb161c0eb489b95a67d029999d9892
pswierzbinski <pawel.swierzbinski22@gmail.com>
2024-01-10 22:31:34 +0100

    Changed project structure

---
+++ src/Pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "../Components/RegisterForm";
+
+function RegisterPage(){
+    return (
+        <div>
+          <RegisterForm></RegisterForm>
+        </div>
+    );
+}
+
+export default RegisterPage;
