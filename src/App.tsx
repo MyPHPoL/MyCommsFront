5520035d6277ab0cfec3936b35b15d241ddd4180
pswierzbinski <pawel.swierzbinski22@gmail.com>
2024-01-10 21:49:24 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App(){
     return (
         <div>
           {/* <RegisterForm/> */}
-          <LoginForm/>
-          {/*<MainPage/>*/} 
+          {/* <LoginForm/> */}
+          <MainPage/> 
         </div>
     );
 }
