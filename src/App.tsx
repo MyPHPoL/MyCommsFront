49ad47f34886d103c9b67cfbad308a7ae4e5e316
pswierzbinski <pawel.swierzbinski22@gmail.com>
2024-01-16 18:18:09 +0100

    routing change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import MainPage from "./Pages/MainPage";
 import LoginPage from "./Pages/LoginPage";
 import RegisterPage from "./Pages/RegisterPage";
+import { Outlet } from "react-router-dom";
 function App(){
     return (
         <div>
-          <LoginPage/> 
+          <Outlet />
         </div>
     );
 }
