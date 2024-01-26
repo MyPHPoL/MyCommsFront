6e50a15f657fe2527922d73eb456848cc85c775f
Syadaisu <129623609+Syadaisu@users.noreply.github.com>
2024-01-26 01:50:32 +0100

    Dropdown and list groups

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,6 +1,6 @@
 
 import { Navigate } from 'react-router-dom';
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import TopbarServer from "./TopbarServer";
 import { IconButton } from "./IconLib";
 import { friends } from "../fakedb";
@@ -19,11 +19,11 @@ import CustomDialog from './DialogTemplate';
 const SERVER_LIST_URL = '/Server/GetServers';
 
 function Header() {
-    const [activeTopbar, setActiveTopbar] = useState<string | null>(null);
-    const { auth }: { auth: any } = useContext(DataContext); // id, username, email, password, token
-    const { setAuth }: { setAuth: any } = useContext(AuthContext);
-    const [servers, setServers] = useState<ServerProps[] | undefined>();
-    const [dialogOpen, setDialogOpen] = useState(false);
+  const [activeTopbar, setActiveTopbar] = useState<string | null>(null);
+  const { auth }: { auth: any } = useContext(DataContext); // id, username, email, password, token
+  const { setAuth }: { setAuth: any } = useContext(AuthContext);
+  const [servers, setServers] = useState<ServerProps[] | undefined>();
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   const handleDialogOpen = () => {
     setDialogOpen(true);
@@ -33,77 +33,77 @@ function Header() {
     setDialogOpen(false);
   };
 
-    // for user authentication (should be moved to axios.ts later)
-    const config = {
-      headers: { Authorization: `Bearer ${auth.token}` } 
+  // for user authentication (should be moved to axios.ts later)
+  const config = {
+    headers: { Authorization: `Bearer ${auth.token}` }
+  };
+
+  useEffect(() => {
+    let isMounted = true; // something, something not to render when component is unmounted
+    const controller = new AbortController(); // cancels request when component unmounts
+
+    const getServers = async () => {
+      try {
+        const response = await axios.get(SERVER_LIST_URL, config);
+        isMounted && setServers(response.data);
+      } catch (error: any) {
+        console.log(error); // TODO: handle error
+      }
     };
 
-    useEffect(() => {
-      let isMounted = true; // something, something not to render when component is unmounted
-      const controller = new AbortController(); // cancels request when component unmounts
-  
-      const getServers = async () => {
-          try {
-              const response = await axios.get(SERVER_LIST_URL, config);
-              isMounted && setServers(response.data);
-          } catch (error: any) {
-              console.log(error); // TODO: handle error
-          }
-      };
-  
-      getServers();
-  
-      return () => {
-          isMounted = false;
-          controller.abort();
-      };
+    getServers();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
   }, []); // WITHOUT THIS ARRAY, IT WILL INFINITELY LOOP
 
-    const logout = async () => {
-      setAuth({}); // clear auth context
-      <Navigate to='/home' />;
-    };
+  const logout = async () => {
+    setAuth({}); // clear auth context
+    <Navigate to='/home' />;
+  };
 
-    return (
-        <div>
-            <nav className="float-left h-20 w-full fixed bg-primary">
-                <ul className="float-left  flex leading-[80px] text-white uppercase">
-                    <li className="float-left text-white text-3xl font-bold leading-[80px] pl-12">{auth.username}</li>
-                    <li className="relative flex items-center justify-center ml-3"><FaRegUser size={30}/></li>
-                    <li className="relative flex items-center justify-center mx-4"></li>
-                    <li className="relative flex items-center justify-center mx-auto" id="dropdown-button">
-                        <button onClick={toggleDropdown}>
-                        <IconButton icon={<IoMdSettings size={30}/>} name="Settings" ></IconButton>
-                        </button>
-                      {/* dropdown that is toggled by above button, currently test values, new content to have tailwind classes like "test" */} 
-                      <div id="dropdown-menu" className="hidden divide-y top-[40px] left-[42px] z-10 divide-primary absolute text-white w-300 border border-gray-900 bg-secondary shadow-md mt-2 rounded-xl text-base">
-                        <div className="py-2 px-2 cursor-pointer hover:text-primary hover:bg-yellow-500 rounded-t-xl text-center">Settings</div>
-                        <div className="py-2 px-2 cursor-pointer hover:text-primary hover:bg-yellow-500 text-center">test</div>
-                        <div className="py-2 px-2 cursor-pointer hover:text-primary hover:bg-yellow-500 rounded-b-xl text-center" onClick={logout}>Log out</div>
-                      </div>
-                    </li>
-                    <label  style={{borderRight: '2px solid grey', borderRadius: '50%', margin: '15px'}}></label>
-                    <li className="relative flex items-center justify-center mx-auto mr-1"><button onClick={() => setActiveTopbar('servers')}><IconButton icon={<IoServer size={30}/>} name="ServerList"></IconButton></button></li>
-                    <li className="relative flex items-center justify-center mx-auto"><button onClick={() => setActiveTopbar('friends')}><IconButton icon={<FaUserFriends size={30}/>} name="FriendList"></IconButton></button></li>
-                    <label  style={{borderRight: '2px solid grey', borderRadius: '50%', margin: '15px'}}></label>
-                    {/* basic dialog window usage */  }
-                    <button onClick={handleDialogOpen}><IconButton icon={<IoMdSettings size={30}/>} name="Dialog" ></IconButton></button>
-                    <CustomDialog open={dialogOpen} handleClose={handleDialogClose} title="My Dialog" content="This is my dialog content." />
-                </ul>
-                <div className="my-2 flex">
-                <SidebarBasic />
-                {activeTopbar === 'servers' && <TopbarServer servers={servers}/>}
-                {activeTopbar === 'friends' && <TopbarFriend items={friends} />}
-                </div>
-            </nav>
+  return (
+    <div>
+      <nav className="float-left h-20 w-full fixed bg-primary">
+        <ul className="float-left  flex leading-[80px] text-white uppercase">
+          <li className="float-left text-white text-3xl font-bold leading-[80px] pl-12">{auth.username}</li>
+          <li className="relative flex items-center justify-center ml-3"><FaRegUser size={30} /></li>
+          <li className="relative flex items-center justify-center mx-4"></li>
+          <li className="relative flex items-center justify-center mx-auto" id="dropdown-button">
+            <button onClick={toggleDropdown}>
+              <IconButton icon={<IoMdSettings size={30} />} name="Settings" ></IconButton>
+            </button>
+            {/* dropdown that is toggled by above button, currently test values, new content to have tailwind classes like "test" */}
+            <div id="dropdown-menu" className="hidden divide-y top-[40px] left-[42px] z-10 divide-primary absolute text-white w-300 border border-gray-900 bg-secondary shadow-md mt-2 rounded-xl text-base">
+              <div className="py-2 px-2 cursor-pointer hover:text-primary hover:bg-yellow-500 rounded-t-xl text-center">Settings</div>
+              <div className="py-2 px-2 cursor-pointer hover:text-primary hover:bg-yellow-500 text-center">test</div>
+              <div className="py-2 px-2 cursor-pointer hover:text-primary hover:bg-yellow-500 rounded-b-xl text-center" onClick={logout}>Log out</div>
+            </div>
+          </li>
+          <label style={{ borderRight: '2px solid grey', borderRadius: '50%', margin: '15px' }}></label>
+          <li className="relative flex items-center justify-center mx-auto mr-1"><button onClick={() => setActiveTopbar('servers')}><IconButton icon={<IoServer size={30} />} name="ServerList"></IconButton></button></li>
+          <li className="relative flex items-center justify-center mx-auto"><button onClick={() => setActiveTopbar('friends')}><IconButton icon={<FaUserFriends size={30} />} name="FriendList"></IconButton></button></li>
+          <label style={{ borderRight: '2px solid grey', borderRadius: '50%', margin: '15px' }}></label>
+          {/* basic dialog window usage */}
+          <button onClick={handleDialogOpen}><IconButton icon={<IoMdSettings size={30} />} name="Dialog" ></IconButton></button>
+          <CustomDialog open={dialogOpen} handleClose={handleDialogClose} title="My Dialog" content="This is my dialog content." />
+        </ul>
+        <div className="my-2 flex">
+          <SidebarBasic />
+          {activeTopbar === 'servers' && <TopbarServer servers={servers} />}
+          {activeTopbar === 'friends' && <TopbarFriend items={friends} />}
         </div>
-    );
-    function toggleDropdown() {
-      const dropdown = document.querySelector('#dropdown-menu');
-      if (dropdown) {
-        dropdown.classList.toggle('hidden');
-      }
+      </nav>
+    </div>
+  );
+  function toggleDropdown() {
+    const dropdown = document.querySelector('#dropdown-menu');
+    if (dropdown) {
+      dropdown.classList.toggle('hidden');
     }
+  }
 }
 
 
--- src/Components/Server.tsx
+++ src/Components/Server.tsx
@@ -8,6 +8,8 @@ import { MdRememberMe } from "react-icons/md";
 import { FaSearch } from "react-icons/fa";
 import { FaRegPlusSquare } from "react-icons/fa";
 import { ChannelButton, IconButton } from "./IconLib";
+import { MdMoreHoriz } from "react-icons/md";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 export interface ServerProps {
   id: string;
@@ -20,11 +22,14 @@ export interface ServerProps {
 
 function Server() {
   const { ServerId } = useParams();
-  console.log(ServerId)
+  console.log(ServerId);
   const Server = test12.find((server) => server.id === ServerId);
   const ServerOwner = users.find((user) => user.id === Server?.ownerId);
   const [showMembers, setShowMembers] = useState(false);
   const [widthmsg, setWidthmsg] = useState(0);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [showChannels, setShowChannels] = useState(false);
+
 
   const Channels = serverChannels.find(
     (channel) => channel.serverId === ServerId
@@ -37,11 +42,48 @@ function Server() {
         ) : null}
         {Server?.name}
         <div className="flex  items-center text-white  text-3xl m-2 mr-0 truncate h-10">
-          <button className='flex-end' onClick={() => {setShowMembers(!showMembers); setWidthmsg(widthmsg === 0 ? 8 : 0)}}>
-                  <IconButton icon={<MdRememberMe size={30}/>} name="ShowMembers"></IconButton>
-          </button>
-          {showMembers && <ServerMembers/>}
+          <div>
+            <button
+              type="button"
+              onClick={() => setDropdownOpen(!dropdownOpen)}
+            >
+              <IconButton
+                icon={<MdMoreHoriz size={30} />}
+                name="ShowMembers"
+              ></IconButton>
+            </button>
           </div>
+
+          {dropdownOpen && (
+            <div className="origin-top-right flex absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-primary ring-1 ring-black ring-opacity-5 ">
+              <div
+                className="py-1"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="options-menu
+                "
+              >
+                <button
+                  className="flex items-center px-4 py-2 text-sm w-full text-white hover:bg-tertiary"
+                  role="menuitem"
+                  onClick={() => {
+                    setShowMembers(!showMembers);
+                    setWidthmsg(widthmsg === 0 ? 8 : 0);
+                  }}
+                >
+                  <MdRememberMe size={25} /> Show Members
+                </button>
+                {showMembers && <ServerMembers />}
+                <button
+                  className="flex items-center px-4 py-2 text-sm w-full text-white hover:bg-tertiary"
+                  role="menuitem"
+                >
+                  <FaRegPlusSquare size={25} />  Add Channel
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
       <div className="text-white text-1xl m-2 truncate">
         {Server?.description}
@@ -50,41 +92,39 @@ function Server() {
         Server created by: {ServerOwner?.name}
       </div>
       <div className="my-1 ml-2 xl:w-auto">
-          <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-              <input type="search" className="relative m-0 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal text-neutral-700 outline-none transition duration-200 dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200" placeholder="Search" />
-              <span className="flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200">
-                  <FaSearch size={20}/>
-              </span>
-          </div>
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <input
+            type="search"
+            className="relative m-0 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal text-neutral-700 outline-none transition duration-200 dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200"
+            placeholder="Search"
+          />
+          <span className="flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200">
+            <FaSearch size={20} />
+          </span>
+        </div>
       </div>
-      <div className="inline-flex items-center justify-center w-full">
-        <hr className="w-64 h-1 mx-auto my-4 bg-gray-200 border-0"></hr>
-        <span className="absolute px-3 font-semibold text-gray-300 -translate-x-1/2 bg-tertiary left-1/2">
-          All Channels
-        </span>
-      </div>
-      <ul>
-        {Channels?.channels.map(({id, name}) => (
-          <li key={id}>
-            <Link to={id}>
-              <div className="justify-left flex flex-col m-1">
-                <button>
-                  <ChannelButton name={`#${name}`} ></ChannelButton>
-                </button>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className="items-center align-center flex flex-col ml-1">
-        <button>
-          <IconButton icon={<FaRegPlusSquare size={30}/>} name="AddChannel"></IconButton>
-        </button>
-      </div>
-              <Routes>
-                <Route path="/:ChannelId/*" element={<Channel widthmsg={widthmsg}/>} />
-              </Routes>
-      </div>
+      <button className="flex m-2 text-white font-semibold" onClick={() => setShowChannels(!showChannels)}>
+        PH Channel Group <IoMdArrowDropdown size='25' />
+      </button>
+      {showChannels && (
+        <ul>
+          {Channels?.channels.map(({ id, name }) => (
+            <li key={id}>
+              <Link to={id}>
+                <div className="justify-left flex flex-col m-1">
+                  <button>
+                    <ChannelButton name={`#${name}`}></ChannelButton>
+                  </button>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+      <Routes>
+        <Route path="/:ChannelId/*" element={<Channel widthmsg={widthmsg} />} />
+      </Routes>
+    </div>
   );
 }
 
