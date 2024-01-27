4089aaa8b64858098ea525f7c11accb55550860a
pswierzbinski <pawel.swierzbinski22@gmail.com>
2024-01-27 23:11:19 +0100

    buttons changed cuz >:(

--- src/Components/TopbarBasic.tsx
+++ src/Components/TopbarBasic.tsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
-import { IoMdCreate } from "react-icons/io";
 import { IconButton } from "./IconLib";
 import CustomDialog from "./DialogTemplate";
 import { ServerProps } from "./Server";
-
+import { GiExitDoor } from "react-icons/gi";
 interface TopBarProps {
   handleAddServer: (server: ServerProps) => void;
 }
@@ -30,10 +29,10 @@ export default function TopbarBasic ({ handleAddServer }: TopBarProps) {
       <div className='h-auto w-auto flex flex-row bg-primary'>
       <ul className='first:my-0 last:my-0 flex flex-row'>
           <i className="mr-1" onClick={() => setDialogTypeAndOpen("Join Server")}>
-            <IconButton icon={<IoMdAdd size='25' />} name={"Join Server"} />
+            <IconButton icon={<GiExitDoor size='25' />} name={"Join Server"} />
           </i>
           <i  onClick={() => setDialogTypeAndOpen("Create Server")}>
-            <IconButton icon={<IoMdCreate size='25' />} name={"Create Server"} />
+            <IconButton icon={<IoMdAdd size='25' />} name={"Create Server"} />
           </i>
         </ul>
         </div>
