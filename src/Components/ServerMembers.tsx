ee4f15b19d24461f00115170599bfb4665ae106b
Syadaisu <129623609+Syadaisu@users.noreply.github.com>
2024-01-28 19:11:25 +0100

    Lot of small changes

--- src/Components/ServerMembers.tsx
+++ src/Components/ServerMembers.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import "../index.css";
 import { UserAvatar } from "./IconLib";
-import { IoMdArrowDropdown } from "react-icons/io";
 import { UserProps } from "./User";
 
 interface ServerMembersProps {
@@ -16,18 +15,18 @@ function ServerMembers({ serverMembers }: ServerMembersProps) {
         <div className="flex m-2 text-white text-xl font-semibold items-center">
           All Members
         </div>
-          <ul>
-            {serverMembers.map((user) => ( // Fix the map function
-              <li key={user.id} className="w-full">
-                <div className="text-lg flex flex-col m-1 mb-2 font-semibold text-white mr-2 pl-2 border-[1px] border-tertiary hover:bg-secondary py-2 px-4 shadow w-full items-start">
-                  <div className="flex justify-start items-center w-full">
-                    <div className="scale-75"><UserAvatar name={user.username}/></div>
-                    <span className="ml-2">{user.username}</span>
-                  </div>
+        <ul>
+          {serverMembers.map((user) => ( // Fix the map function
+            <li key={user.id} className="w-full">
+              <div className="text-lg flex flex-col my-1 mb-2 font-semibold text-white mr-2 pl-2 border-[1px] border-tertiary hover:bg-secondary py-2 px-4 shadow w-full items-start">
+                <div className="flex justify-start items-center w-full">
+                  <div className="scale-75"><UserAvatar name={user.username} /></div>
+                  <span className="ml-2">{user.username}</span>
                 </div>
-              </li>
-            ))}
-          </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
