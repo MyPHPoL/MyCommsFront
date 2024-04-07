e4ff2222206f88438e4f1b0b19a3c51241e7a9b7
Syadaisu <daszymanski001@gmail.com>
2024-04-07 11:24:10 +0200

    TopBar changes for private message feature

--- src/Components/TopbarFriend.tsx
+++ src/Components/TopbarFriend.tsx
@@ -1,18 +1,55 @@
 import React from "react";
 import { Route, Link, Routes } from "react-router-dom";
 import { ServerButton } from "./IconLib";
+import { useState } from "react";
+import CustomDialog from "./DialogTemplate";
+import { TiUserAdd } from "react-icons/ti";
+import { IconButton } from "./IconLib";
 
 import FriendMessage, { FriendProps } from "./FriendMessage";
 
 interface TopbarProps {
   friends?: FriendProps[];
+  //handleAddFriend: (friend: FriendProps) => void;
+  //removeFriend: (id: string) => void;
 }
 
 export default function TopbarFriend({ friends }: TopbarProps) {
+
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [dialogType, setDialogType] = useState("Add Friend");
+
+  const handleDialogOpen = () => {
+    setDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+
+  const setDialogTypeAndOpen = (type: string) => {
+    setDialogType(type);
+    handleDialogOpen();
+  };
+
   return (
     <div>
-      <div className='h-auto w-auto flex flex-row bg-primary'>
-        <ul className='m-4 first:my-0 last:my-0 flex flex-row'>
+      <div className="h-auto w-auto flex flex-row bg-primary">
+        <ul className="first:my-0 last:my-0 flex flex-row">
+          <i
+            className="mr-1"
+            onClick={() => setDialogTypeAndOpen("Join Server")}
+          >
+            <IconButton icon={<TiUserAdd size="30" />} name={"Add Friend"} />
+          </i>
+        </ul>
+        <CustomDialog
+          open={dialogOpen}
+          handleClose={handleDialogClose}
+          type={dialogType}
+          //handleAddFriend={handleAddFriend}
+        />
+        <ul className="m-4 first:my-0 last:my-0 flex flex-row">
           {friends?.map(({ id, username, picture }) => (
             <li className="mr-1" key={id}>
               <Link to={id}>
@@ -23,7 +60,7 @@ export default function TopbarFriend({ friends }: TopbarProps) {
         </ul>
       </div>
       <Routes>
-        <Route path='/:UserId/*' element={<FriendMessage />} />
+        <Route path="/:UserId/*" element={<FriendMessage />} />
       </Routes>
     </div>
   );
--- src/Components/TopbarServer.tsx
+++ src/Components/TopbarServer.tsx
@@ -1,23 +1,67 @@
 import React from "react";
+import { useState } from "react";
 import { Route, Link, Routes } from "react-router-dom";
 import { ServerButton } from "./IconLib";
+import { FaDoorOpen } from "react-icons/fa6";
+import { IconButton } from "./IconLib";
+import { IoMdAdd } from "react-icons/io";
+
+import CustomDialog from "./DialogTemplate";
 
 import Server, { ServerProps } from "./Server";
 
 interface TopbarProps {
   servers?: ServerProps[];
   removeServer: (id: string) => void;
+  handleAddServer: (server: ServerProps) => void;
 }
 
-export default function TopbarServer({ servers, removeServer }: TopbarProps) {
+export default function TopbarServer({
+  servers,
+  removeServer,
+  handleAddServer,
+}: TopbarProps) {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [dialogType, setDialogType] = useState("Create Server");
+
+  const handleDialogOpen = () => {
+    setDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+
+  const setDialogTypeAndOpen = (type: string) => {
+    setDialogType(type);
+    handleDialogOpen();
+  };
 
   return (
     <div>
-      <div className='h-auto w-auto flex flex-row bg-primary'>
-        <ul className='m-4 first:my-0 last:my-0 flex flex-row'>
+      <div className="h-auto w-auto flex flex-row bg-primary">
+        <ul className="first:my-0 last:my-0 flex flex-row">
+          <i
+            className="mr-1"
+            onClick={() => setDialogTypeAndOpen("Join Server")}
+          >
+            <IconButton icon={<FaDoorOpen size="25" />} name={"Join Server"} />
+          </i>
+          <i onClick={() => setDialogTypeAndOpen("Create Server")}>
+            <IconButton icon={<IoMdAdd size="25" />} name={"Create Server"} />
+          </i>
+        </ul>
+        <CustomDialog
+          open={dialogOpen}
+          handleClose={handleDialogClose}
+          type={dialogType}
+          handleAddServer={handleAddServer}
+        />
+
+        <ul className="m-4 first:my-0 last:my-0">
           {servers?.map(({ id, name, picture }) => (
             <li className="mr-2" key={id}>
-              <Link to={'/home/'+id}>
+              <Link to={"/home/" + id}>
                 <ServerButton name={name} picture={picture} />
               </Link>
             </li>
@@ -25,10 +69,11 @@ export default function TopbarServer({ servers, removeServer }: TopbarProps) {
         </ul>
       </div>
       <Routes>
-        <Route path='/:ServerId/*' element={<Server removeServer={removeServer}/>} />
+        <Route
+          path="/:ServerId/*"
+          element={<Server removeServer={removeServer} />}
+        />
       </Routes>
     </div>
   );
 }
-
-
