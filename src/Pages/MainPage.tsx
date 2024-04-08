65d6726e6ffd8c1874bb21bb5cf25d2416de3b17
piotus <piotrradziszewski@outlook.com>
2024-04-08 18:48:27 +0200

    persistent login information + splitted user settings to a new component

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,9 +1,34 @@
 import Header from "../Components/Header";
+import { useEffect, useState } from "react";
+import useAuth from "../Hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 function MainPage(){
+    const [isLoading, setIsLoading] = useState(true);
+    const {setAuth} = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem("auth");
+        if (loggedInUser) {
+            const foundUser = JSON.parse(loggedInUser);
+            setAuth(foundUser);
+            setIsLoading(false);
+        }
+        else {
+            navigate("/login");
+        }
+      }, []);
+
     return (
-        <div className='flex'>
-            <Header></Header>
+        <div className="flex">
+            {isLoading ? (
+                <div/>
+            ) : (
+                <div>
+                    <Header></Header>
+                </div>
+            )}
         </div>
     );
 }
