a16070cd53c40f8e5bb7b4791f1939751164d082
pswierzbinski <pawel.swierzbinski22@gmail.com>
2024-04-29 20:11:25 +0200

    embeded youtube links

--- src/Components/Message.tsx
+++ src/Components/Message.tsx
@@ -8,16 +8,19 @@ export const Message = ({ authorId, body, creationDate, attachment }: MessagePro
   const { auth }: { auth: any } = useAuth(); // id, username, email, password, token
   const getFileUrl = (attachment: string) => `https://localhost:7031/file/${attachment}`;
   const gifRegex: RegExp = /^(https\:\/\/media\.tenor\.com\/).*(\.gif)/g; //check if the sent message fits tenor gif format
-
-  // get attachment from attachment
-
-  // get username from authorId
+  const youtubeRegex: RegExp = /(^https:\/\/www\.youtube\.com\/watch\?v=).*/g; //check if the sent message fits youtube video format
+  const shortYoutubeRegex: RegExp = /(^https:\/\/youtu\.be\/).*/g; //check if the sent message fits mobile youtube video format (needs to be converted in order to work)
+  var isYoutube: boolean = body.match(youtubeRegex) ? true : false;
+  if(shortYoutubeRegex.test(body)){
+    body = body.replace('youtu.be/', 'youtube.com/watch?v=');
+    isYoutube = true;
+  }
   getUsername(auth.token, authorId).then((res) => {
     const username: string = res;
     setUsername(username);
   });
   const isGif: boolean = body.match(gifRegex) ? true : false;
-
+  
   if (isGif) {
     return (
       <div className='w-full flex-row justify-evenly py-3 px-8 m-0 cursor-pointer border-tertiary border-b-2 hover:bg-tertiary'>
@@ -47,6 +50,20 @@ export const Message = ({ authorId, body, creationDate, attachment }: MessagePro
             {body}
           </p>
           {attachment && <img loading="lazy" src={getFileUrl(attachment)} className='w-1/4 object-scale-down max-h-96 max-w-96' alt="attachement"/>}
+        </div>
+      </div>
+    )
+  } else if (isYoutube) {
+    return (
+      <div className='w-full flex-row justify-evenly py-3 px-8 m-0 cursor-pointer border-tertiary border-b-2 hover:bg-tertiary'>
+        <div className='flex flex-col justify-start ml-auto border-tertiary'>
+          <p className='text-left font-semibold text-white mr-2 cursor-pointer'>
+            {username}
+            <small className='text-xs text-left font-semibold text-gray-500 ml-2'>
+              {new Date(creationDate).toLocaleDateString()} {new Date(creationDate).toLocaleTimeString()}
+            </small>
+          </p>
+          <iframe loading="lazy" width="560" height="315" src={body.replace('watch?v=', 'embed/')} allowFullScreen></iframe>
         </div>
       </div>
     )
