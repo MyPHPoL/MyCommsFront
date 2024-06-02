9fcc24874ef6d8b815b3887f59a5145fbd1525fb
Syadaisu <129623609+Syadaisu@users.noreply.github.com>
2024-06-02 22:53:24 +0200

    Small update

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -14,93 +14,182 @@ import { TiUserAdd } from "react-icons/ti";
 import AddFriendDialog from "./DialogPopups/AddFriendDialog";
 
 interface DashBoardProps {
-    friends: FriendProps[] | undefined;
-    servers: ServerProps[] | undefined;
-    mode: string;
-    removeServer: (id: string) => void;
-    handleAddServer: (server: ServerProps) => void;
-  }
-  
-  function Dashboard({ friends, servers, removeServer, mode, handleAddServer }: DashBoardProps) {
+  friends: FriendProps[] | undefined;
+  servers: ServerProps[] | undefined;
+  mode: string;
+  removeServer: (id: string) => void;
+  handleAddServer: (server: ServerProps) => void;
+}
 
-    const RenderFriends: React.FC<DashBoardProps> = ({ friends = [] }) => {
-      const [filteredFriends, setFilteredFriends] = useState(friends);
-      const [addOpen, setAddOpen] = useState(false);
+function Dashboard({ friends, servers, removeServer, mode, handleAddServer }: DashBoardProps) {
+
+  const RenderFriends: React.FC<DashBoardProps> = ({ friends = [] }) => {
+    const [filteredFriends, setFilteredFriends] = useState(friends);
+    const [incInvites, setIncInvites] = useState([]);
+    const [outInvites, setOutInvites] = useState([]);
+    const [addOpen, setAddOpen] = useState(false);
 
 
-      const handleFilter = (event: any) => {
-        const value = event.target.value;
-        const filtered = friends.filter(friend => friend.username.includes(value));
-        setFilteredFriends(filtered);
-      };
+    const handleFilter = (event: any) => {
+      const value = event.target.value;
+      const filtered = friends.filter(friend => friend.username.includes(value));
+      setFilteredFriends(filtered);
+    };
 
-      const handleAddOpen = () => {
-        setAddOpen(true);
-      };
-  
-      const handleAddClose = () => {
-        setAddOpen(false);
-      };
-   
-      
+    const handleAddOpen = () => {
+      setAddOpen(true);
+    };
 
-      return (
-      <div className="w-[720px]">
-      <h1 className='text-5xl font-bold my-4 text-white mx-2 pl-[20px]'> Your Friends </h1>
-      <div className='relative w-[600px] h-12 my-2 pl-[20px]'>
-        <input
-            onChange={handleFilter}
-            type='text'
-            placeholder='Filter your friends by their name...'
-            autoComplete='off'
-            className='placeholder:color-white w-[600px] text-white h-full border-2 border-solid border-slate-600 bg-transparent outline-none color-white rounded-s-3xl rounded-e-3xl pt-5 pr-11 pb-5 pl-5'
-        />
-      </div>
-      <div className='overflow-y-auto h-[700px]'>
-      {filteredFriends?.map(({ id, username, picture }) => (
-        <div
-          key={id}
-          className='group flex-row flex w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white items-center'>
-          <Link to={`/server/${id}`}>
-            <div className='relative flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] hover:bg-yellow-500 hover:text-primary align-middle duration-300 ease-linear'>
-              <UserAvatar
-                name={username}
-                picture={
-                  picture
-                    ? "https://localhost:7031/file/" + picture
-                    : undefined
-                }
-              />
-              <div className='pl-2'>
-                {username}
-                <div className='text-base font-normal overflow-clip'></div>
-              </div>
-            </div>
-          </Link>
-        </div>
-      ))}
-      <div className="w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white">
-        <button className='flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] border-dashed border-2 hover:bg-yellow-500 hover:text-primary duration-300 ease-linear'
-        onClick={() => handleAddOpen()}>
-          <TiUserAdd className="mx-3" size="32" />
-          <div className='pl-2'>
-            <div className="float-left">Click here to add a friend!</div>
-            <div className='text-base font-normal overflow-clip float-left'>All you need is their username!</div>        
+    const handleAddClose = () => {
+      setAddOpen(false);
+    };
+
+
+
+    return (
+      <div className="flex">
+        <div className="w-[720px]">
+          <h1 className='text-5xl font-bold my-4 text-white mx-2 pl-[20px]'> Your Friends </h1>
+          <div className='relative w-[600px] h-12 my-2 pl-[20px]'>
+            <input
+              onChange={handleFilter}
+              type='text'
+              placeholder='Filter your friends by their name...'
+              autoComplete='off'
+              className='placeholder:color-white w-[600px] text-white h-full border-2 border-solid border-slate-600 bg-transparent outline-none color-white rounded-s-3xl rounded-e-3xl pt-5 pr-11 pb-5 pl-5'
+            />
           </div>
-        </button>
-      </div>
-      
-    </div>
-      <AddFriendDialog
-        open={addOpen}
-        handleClose={handleAddClose}
-        />
-    </div>
-      );
-    }
-  
+          <div className='overflow-y-auto h-[700px]'>
+            {filteredFriends?.map(({ id, username, picture }) => (
+              <div
+                key={id}
+                className='group flex-row flex w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white items-center'>
+                <Link to={`/friends/${id}`}>
+                  <div className='relative flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] hover:bg-yellow-500 hover:text-primary align-middle duration-300 ease-linear'>
+                    <UserAvatar
+                      name={username}
+                      picture={
+                        picture
+                          ? "https://localhost:7031/file/" + picture
+                          : undefined
+                      }
+                    />
+                    <div className='pl-2'>
+                      {username}
+                      <div className='text-base font-normal overflow-clip'></div>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+            <div className="w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white">
+              <button className='flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] border-dashed border-2 hover:bg-yellow-500 hover:text-primary duration-300 ease-linear'
+                onClick={() => handleAddOpen()}>
+                <TiUserAdd className="mx-3" size="32" />
+                <div className='pl-2'>
+                  <div className="float-left">Click here to add a friend!</div>
+                  <div className='text-base font-normal overflow-clip float-left'>All you need is their username!</div>
+                </div>
+              </button>
+            </div>
 
-    const RenderServers: React.FC<DashBoardProps> = ({ servers = [], removeServer, handleAddServer }) => {
+          </div>
+          <AddFriendDialog
+            open={addOpen}
+            handleClose={handleAddClose}
+          />
+        </div>
+
+
+        <div className="w-[720px]">
+          <h1 className='text-5xl font-bold my-4 text-white mx-2 pl-[20px]'> Your Incoming Invites </h1>
+          <div className='relative w-[600px] h-12 my-2 pl-[20px]'>
+            <input
+              onChange={handleFilter}
+              type='text'
+              placeholder='Filter your inivtes by the invitee...'
+              autoComplete='off'
+              className='placeholder:color-white w-[600px] text-white h-full border-2 border-solid border-slate-600 bg-transparent outline-none color-white rounded-s-3xl rounded-e-3xl pt-5 pr-11 pb-5 pl-5'
+            />
+          </div>
+          <div className='overflow-y-auto h-[700px]'>
+            {outInvites?.map(({ id, username, picture, creationDate }) => (
+              <div
+                key={id}
+                className='group flex-row flex w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white items-center'>
+                <div className='relative flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] hover:bg-yellow-500 hover:text-primary align-middle duration-300 ease-linear'>
+                  <UserAvatar
+                    name={username}
+                    picture={
+                      picture
+                        ? "https://localhost:7031/file/" + picture
+                        : undefined
+                    }
+                  />
+                  <div className='pl-2'>
+                    {username}
+                    <small className='text-xs text-left font-semibold text-gray-500 ml-2'>
+                      {new Date(creationDate).toLocaleDateString()} {new Date(creationDate).toLocaleTimeString()}
+                    </small>
+                    <div className='text-base font-normal overflow-clip'></div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+
+
+        <div className="w-[720px]">
+          <h1 className='text-5xl font-bold my-4 text-white mx-2 pl-[20px]'> Your Outgoing Invites </h1>
+          <div className='relative w-[600px] h-12 my-2 pl-[20px]'>
+            <input
+              onChange={handleFilter}
+              type='text'
+              placeholder='Filter your inivtes by the receiver...'
+              autoComplete='off'
+              className='placeholder:color-white w-[600px] text-white h-full border-2 border-solid border-slate-600 bg-transparent outline-none color-white rounded-s-3xl rounded-e-3xl pt-5 pr-11 pb-5 pl-5'
+            />
+          </div>
+          <div className='overflow-y-auto h-[700px]'>
+            {outInvites?.map(({ id, username, picture, creationDate }) => (
+              <div
+                key={id}
+                className='group flex-row flex w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white items-center'>
+                <Link to={`/friends/${id}`}>
+                  <div className='relative flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] hover:bg-yellow-500 hover:text-primary align-middle duration-300 ease-linear'>
+                    <UserAvatar
+                      name={username}
+                      picture={
+                        picture
+                          ? "https://localhost:7031/file/" + picture
+                          : undefined
+                      }
+                    />
+                    <div className='pl-2'>
+                      {username}
+                      <small className='text-xs text-left font-semibold text-gray-500 ml-2'>
+                        {new Date(creationDate).toLocaleDateString()} {new Date(creationDate).toLocaleTimeString()}
+                      </small>
+                      <div className='text-base font-normal overflow-clip'></div>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+
+      </div>
+
+
+
+    );
+  }
+
+
+  const RenderServers: React.FC<DashBoardProps> = ({ servers = [], removeServer, handleAddServer }) => {
 
     const [filteredServers, setFilteredServers] = useState(servers);
     const [serverDeleteOpen, setServerDeleteOpen] = useState(false);
@@ -135,7 +224,7 @@ interface DashBoardProps {
       setPassedId(id);
       setServerDeleteOpen(true);
     }
-    
+
     const handleServerDeleteClose = () => {
       setServerDeleteOpen(false);
     }
@@ -150,97 +239,97 @@ interface DashBoardProps {
     const handleCreateOpen = () => {
       setCreateOpen(true);
     };
-  
+
     const handleCreateClose = () => {
       setCreateOpen(false);
     };
 
     return (
-    <div className="w-[720px]">
-      <h1 className='text-5xl font-bold my-4 text-white mx-2 pl-[20px]'> Your Servers </h1>
-      <div className='relative w-[600px] h-12 my-2 pl-[20px]'>
-        <input
+      <div className="w-[720px]">
+        <h1 className='text-5xl font-bold my-4 text-white mx-2 pl-[20px]'> Your Servers </h1>
+        <div className='relative w-[600px] h-12 my-2 pl-[20px]'>
+          <input
             onChange={handleFilter}
             type='text'
             placeholder='Filter servers by name...'
             autoComplete='off'
             className='placeholder:color-white w-[600px] text-white h-full border-2 border-solid border-slate-600 bg-transparent outline-none color-white rounded-s-3xl rounded-e-3xl pt-5 pr-11 pb-5 pl-5'
+          />
+        </div>
+        <div className='overflow-y-auto h-[700px]'>
+          {filteredServers?.map(({ id, name, description, picture, ownerId }) => (
+            <div
+              key={id}
+              className='group flex-row flex w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white items-center'>
+              <Link to={`/server/${id}`}>
+                <div className='relative flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] hover:bg-yellow-500 hover:text-primary align-middle duration-300 ease-linear'>
+                  <UserAvatar
+                    name={name}
+                    picture={
+                      picture
+                        ? "https://localhost:7031/file/" + picture
+                        : undefined
+                    }
+                  />
+                  <div className='pl-2'>
+                    {name}
+                    <div className='text-base font-normal overflow-clip'>{description}</div>
+                  </div>
+                </div>
+              </Link>
+              {auth.id === ownerId &&
+                <button className="invisible group-hover:visible px-4 py-2 ml-0 text-sm text-white rounded-full bg-tertiary hover:bg-red-600 transition-all duration-300 ease-linear"
+                  onClick={() => handleServerDeleteOpen(id)}>
+                  <MdDeleteForever size={25} />
+                </button>
+              }
+            </div>
+          ))}
+          <div className="w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white">
+            <button className='flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] border-dashed border-2 hover:bg-yellow-500 hover:text-primary duration-300 ease-linear'
+              onClick={() => handleJoinOpen()}>
+              <FaDoorOpen className="mx-3" size="32" />
+              <div className='pl-2'>
+                <div className="float-left">Click here to join a server!</div>
+                <div className='text-base font-normal overflow-clip float-left'>All you need is a server name!</div>
+              </div>
+            </button>
+          </div>
+          <div className="w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white">
+            <button className='flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] border-dashed border-2 hover:bg-yellow-500 hover:text-primary duration-300 ease-linear'
+              onClick={() => handleCreateOpen()}>
+              <IoMdAdd className="mx-3" size="32" />
+              <div className='pl-2'>
+                <div className="float-left">Click here to create a new server!</div>
+                <div className='text-base font-normal overflow-clip float-left'>Perfect new server just a click away!</div>
+              </div>
+            </button>
+          </div>
+        </div>
+        <DeleteServerConfirmation
+          open={serverDeleteOpen}
+          handleClose={handleServerDeleteClose}
+          removeServer={removeServer}
+          passedId={dialogId} />
+        <CreateServerDialog
+          open={createOpen}
+          handleClose={handleCreateClose}
+          handleAddServer={handleAddServer}
+        />
+        <JoinServerDialog
+          open={joinOpen}
+          handleClose={handleJoinClose}
+          handleJoinServer={handleAddServer}
         />
       </div>
-      <div className='overflow-y-auto h-[700px]'>
-      {filteredServers?.map(({ id, name, description, picture, ownerId }) => (
-        <div
-          key={id}
-          className='group flex-row flex w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white items-center'>
-          <Link to={`/server/${id}`}>
-            <div className='relative flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] hover:bg-yellow-500 hover:text-primary align-middle duration-300 ease-linear'>
-              <UserAvatar
-                name={name}
-                picture={
-                  picture
-                    ? "https://localhost:7031/file/" + picture
-                    : undefined
-                }
-              />
-              <div className='pl-2'>
-                {name}
-                <div className='text-base font-normal overflow-clip'>{description}</div>
-              </div>
-            </div>
-          </Link>
-          { auth.id === ownerId &&
-          <button className="invisible group-hover:visible px-4 py-2 ml-0 text-sm text-white rounded-full bg-tertiary hover:bg-red-600 transition-all duration-300 ease-linear"
-                  onClick={() => handleServerDeleteOpen(id)}>
-                  <MdDeleteForever size={25} /> 
-          </button>
-          }
-        </div>
-      ))}
-      <div className="w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white">
-        <button className='flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] border-dashed border-2 hover:bg-yellow-500 hover:text-primary duration-300 ease-linear'
-        onClick={() => handleJoinOpen()}>
-          <FaDoorOpen className="mx-3" size="32" />
-          <div className='pl-2'>
-            <div className="float-left">Click here to join a server!</div>
-            <div className='text-base font-normal overflow-clip float-left'>All you need is a server name!</div>        
-          </div>
-        </button>
-      </div>
-      <div className="w-full pt-2 pb-4 pl-[20px] h-auto text-2xl font-semibold text-white">
-        <button className='flex mr-2 bg-tertiary p-2 rounded-full items-center px-5 w-[600px] border-dashed border-2 hover:bg-yellow-500 hover:text-primary duration-300 ease-linear'
-        onClick={() => handleCreateOpen()}>
-          <IoMdAdd className="mx-3" size="32" />
-          <div className='pl-2'>
-              <div className="float-left">Click here to create a new server!</div>
-              <div className='text-base font-normal overflow-clip float-left'>Perfect new server just a click away!</div>   
-          </div>
-        </button>
-      </div>
-    </div>
-      <DeleteServerConfirmation 
-        open={serverDeleteOpen} 
-        handleClose={handleServerDeleteClose} 
-        removeServer={removeServer} 
-        passedId={dialogId} />
-      <CreateServerDialog
-        open={createOpen}
-        handleClose={handleCreateClose}
-        handleAddServer={handleAddServer}
-      />
-      <JoinServerDialog
-        open={joinOpen}
-        handleClose={handleJoinClose}
-        handleJoinServer={handleAddServer}
-      />
-    </div>
     );
-}
+  }
 
-return (
-  <div className='md:flex h-full w-full -z-20 flex-col fixed inset-y-0 top-20 left-0'>
-    {mode === "friends" ? <RenderFriends {...{friends, servers, removeServer, mode, handleAddServer}} /> : null }
-    {mode === "servers" ? <RenderServers {...{friends, servers, removeServer, mode, handleAddServer}} /> : null }
-  </div>
-);
+  return (
+    <div className='md:flex h-full w-full -z-20 flex-col fixed inset-y-0 top-20 left-0'>
+      {mode === "friends" ? <RenderFriends {...{ friends, servers, removeServer, mode, handleAddServer }} /> : null}
+      {mode === "servers" ? <RenderServers {...{ friends, servers, removeServer, mode, handleAddServer }} /> : null}
+    </div>
+  );
 };
 export default Dashboard;
